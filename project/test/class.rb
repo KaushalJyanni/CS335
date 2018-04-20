f5bb42b1de69f108e23b1681ef75dc821ee92da1
ManthanSit <manthansit0@gmail.com>
2018-04-20 17:00:23 +0530

    test files

--- project/test/class.rb
+++ project/test/class.rb
@@ -1,9 +1,20 @@
-class m
-    x = 1
-    y = 2 
-end
+class a
+    x = 5
+    y = 6
+    def foo(q)
+        z =q +2
+        return z
+    end
+endclass
 
-main
-a = newclass m
-a.x = a.y
-print(a.x)
+f = newclass a
+g = newclass a
+g.y = 1
+p = 3
+f.x = 2
+f.y = f.x 
+f.x = f.foo(g.y)
+print(f.x)
+v=f.foo(g.y)
+f.y = f.foo(v)
+print(f.y)
